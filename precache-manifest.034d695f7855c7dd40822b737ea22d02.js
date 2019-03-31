a8b8c856f52ce2afa50a189bb49c723136970185
martini <zhao435021640@gmail.com>
2019-03-31 19:07:59 +0800

    Updates

---
+++ precache-manifest.034d695f7855c7dd40822b737ea22d02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3d21500ee772c8970c7",
+    "url": "/taggie/static/css/main.c78f2898.chunk.css"
+  },
+  {
+    "revision": "e3d21500ee772c8970c7",
+    "url": "/taggie/static/js/main.a366a962.chunk.js"
+  },
+  {
+    "revision": "40baace4f5ece6edb9be",
+    "url": "/taggie/static/js/runtime~main.9421bc00.js"
+  },
+  {
+    "revision": "302533ffc36ac38145a8",
+    "url": "/taggie/static/css/2.ec700c6e.chunk.css"
+  },
+  {
+    "revision": "302533ffc36ac38145a8",
+    "url": "/taggie/static/js/2.b4bc1728.chunk.js"
+  },
+  {
+    "revision": "ba2c7bc59f0159c0f71015466311120f",
+    "url": "/taggie/static/media/excel with content.ba2c7bc5.png"
+  },
+  {
+    "revision": "27b70737138e3481e2a47df4c2f365d2",
+    "url": "/taggie/static/media/browser.27b70737.png"
+  },
+  {
+    "revision": "a7c092e1efbdd2ab021d12738bf7e6bb",
+    "url": "/taggie/static/media/box.a7c092e1.jpg"
+  },
+  {
+    "revision": "42fa0ca4d7fff480536a9cf492c9b592",
+    "url": "/taggie/static/media/box-pile.42fa0ca4.jpg"
+  },
+  {
+    "revision": "440daeb605b3664af9a16c6aa7408d56",
+    "url": "/taggie/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/taggie/en/precache-manifest.9d9e4cb61f698821fc9ed7c32da5c24d.js"
+  "/taggie/precache-manifest.034d695f7855c7dd40822b737ea22d02.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/taggie/en/index.html", {
+workbox.routing.registerNavigationRoute("/taggie/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
